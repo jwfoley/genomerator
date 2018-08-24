0264c1835540060d63ac0e7a582e0123d9ed98e5
Joe Foley <jwfoley@stanford.edu>
2018-08-24 14:52:38 -0700

    FeatureStream always uses generators

--- genomerator/Input.py
+++ genomerator/Input.py
@@ -12,7 +12,7 @@ class FeatureStream (object):
 	optionally filter the data to only return certain features
 	'''
 	
-	__slots__ = 'source', 'default_data', 'filter', 'verify_order', 'fixed_references', '_reference_lookup', '_previous_feature', 'count_pass', 'count_fail'
+	__slots__ = 'source', 'default_data', 'filter', 'verify_order', 'fixed_references', '_reference_lookup', '_feature_generator', '_previous_feature', 'count_pass', 'count_fail'
 	
 	def __init__ (self,
 		source,
@@ -29,6 +29,7 @@ class FeatureStream (object):
 		self._previous_feature = None
 		self.count_pass = 0
 		self.count_fail = 0
+		self._feature_generator = self._filter_features()
 		if references is None:
 			self.fixed_references = False
 			self._reference_lookup = collections.OrderedDict()
@@ -51,25 +52,25 @@ class FeatureStream (object):
 				self._reference_lookup[reference_name] = reference_id
 				return reference_id
 		
-	def _get_feature (self):
+	def _yield_features (self):
 		'''
 		this exists mainly to be replaced in subclasses
-		should raise StopIteration when it reaches the end
 		'''
-		return next(self.source)
+		for feature in self.source: yield feature
 	
-	def _try_until_pass (self):
-		feature = self._get_feature()
-		while not self.filter(feature):
-			self.count_fail += 1
-		self.count_pass += 1
-		if self.default_data is None:
-			return feature
-		else:
-			return GenomeFeature.from_genomefeature(feature, self.default_data)
+	def _filter_features (self):
+		for feature in self._yield_features():
+			if not self.filter(feature):
+				self.count_fail += 1
+			else:
+				self.count_pass += 1
+				if self.default_data is None:
+					yield feature
+				else:
+					yield GenomeFeature.from_genomefeature(feature, self.default_data)
 	
 	def __next__ (self):
-		new_feature = self._try_until_pass()
+		new_feature = next(self._feature_generator)
 		if self.verify_order and not (self._previous_feature is None or new_feature >= self._previous_feature): raise RuntimeError('input is not properly sorted in item %i' % self.count)
 		self._previous_feature = new_feature
 		return new_feature
@@ -92,14 +93,15 @@ class SamStream (FeatureStream):
 	
 	__slots__ = 'unaligned'
 	
-	def _get_feature (self):
-		alignment = next(self.source)
-		while alignment.is_unmapped:
-			self.unaligned += 1
-			alignment = next(self.source)
-		if self.fixed_references and not alignment.reference_id == self._reference_lookup[alignment.reference_name]:
-			raise RuntimeError('wrong reference ID for alignment %s' % alignment.query_name)
-		return GenomeFeature.from_alignedsegment(alignment)
+	def _yield_features (self):
+		for alignment in self.source:
+			if alignment.is_unmapped:
+				self.unaligned += 1
+			else:
+				if self.fixed_references and not alignment.reference_id == self._reference_lookup[alignment.reference_name]:
+					raise RuntimeError('wrong reference ID for alignment %s' % alignment.query_name)
+				else:
+					yield GenomeFeature.from_alignedsegment(alignment)
 
 
 class VariantStream (FeatureStream):
@@ -107,28 +109,32 @@ class VariantStream (FeatureStream):
 	given an iterable of pysam.VariantRecord instances (e.g. a pysam.VariantFile), yield GenomeFeatures
 	'''
 	
-	def _get_feature (self):
-		variant_record = next(self.source)
-		if self.fixed_references and not variant_record.rid == self._reference_lookup[variant_record.chrom]:
-			raise RuntimeError('wrong reference ID for variant record %s' % variant_record.id)
-		return GenomeFeature.from_variantrecord(variant_record)
+	def _yield_features (self):
+		for variant_record in self.source:
+			if self.fixed_references and not variant_record.rid == self._reference_lookup[variant_record.chrom]:
+				raise RuntimeError('wrong reference ID for variant record %s' % variant_record.id)
+			else:
+				yield GenomeFeature.from_variantrecord(variant_record)
 
 
 class BedStream (FeatureStream):
 	'''
 	given an iterable of BED-format lines (e.g. an opened BED file), yield GenomeFeatures
 	'''
-	def _get_feature (self):
-		fields = next(self.source).rstrip().split()
-		while fields[0].startswith('#') or len(fields) < 3: # skip bad lines
-			fields = next(self.source).rstrip().split()
-		return GenomeFeature(
-			reference_id =  self._get_reference_id(fields[0]),
-			left_pos =      int(fields[1]) + 1,
-			right_pos =     int(fields[2]),
-			is_reverse =    len(fields) >= 6 and fields[5] == '-',
-			data =          fields
-		)
+	def _yield_features (self):
+		for line in self.source:
+			if line.startswith('#') or line.startswith('track') or line.startswith('browser'): continue
+			line = line.rstrip()
+			if len(line) == 0: continue
+			fields = line.split()
+			if len(fields) < 3: raise RuntimeError('bad format:\n%s' % line)
+			yield GenomeFeature(
+				reference_id =  self._get_reference_id(fields[0]),
+				left_pos =      int(fields[1]) + 1,
+				right_pos =     int(fields[2]),
+				is_reverse =    len(fields) >= 6 and fields[5] == '-',
+				data =          fields
+			)
 
 
 class BedgraphStream (FeatureStream):
@@ -137,17 +143,19 @@ class BedgraphStream (FeatureStream):
 	yields each genome position separately, even if file represents continguous runs, unless you specify split_spans = False
 	'''
 	
-	__slots__ = 'split_spans', '_feature_generator'
+	__slots__ = 'split_spans'
 	
 	def __init__ (self, *args, split_spans = True, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.split_spans = split_spans
-		self._feature_generator = self._yield_features()
 	
 	def _yield_features (self):
 		for line in self.source:
-			fields = line.rstrip().split()
-			if len(fields) != 4 or fields[0].startswith('#') or fields[0] == 'track': continue
+			if line.startswith('#') or line.startswith('track') or line.startswith('browser'): continue
+			line = line.rstrip()
+			if len(line) == 0: continue
+			fields = line.split()
+			if len(fields) != 4: raise RuntimeError('bad format:\n%s' % line)
 			full_feature = GenomeFeature(
 				reference_id =  self._get_reference_id(fields[0]),
 				left_pos =      int(fields[1]) + 1,
@@ -159,9 +167,6 @@ class BedgraphStream (FeatureStream):
 			else:
 				yield full_feature
 	
-	def _get_feature (self):
-		return next(self._feature_generator)
-
 
 class WiggleStream (FeatureStream):
 	'''
@@ -169,17 +174,18 @@ class WiggleStream (FeatureStream):
 	yields each genome position separately, even if file represents continguous runs, unless you specify split_spans = False
 	'''
 	
-	__slots__ = 'split_spans', '_format', '_step', '_span', '_start', '_reference_id', '_count_since_header', '_feature_generator'
+	__slots__ = 'split_spans', '_format', '_step', '_span', '_start', '_reference_id', '_count_since_header'
 	
 	def __init__ (self, *args, split_spans = True, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.split_spans = split_spans
-		self._feature_generator = self._yield_features()
 	
 	def _yield_features (self):
 		for line in self.source:
-			fields = line.rstrip().split()
-			if len(fields) == 0 or fields[0].startswith('#') or fields[0] == 'track': continue
+			if line.startswith('#') or line.startswith('track') or line.startswith('browser'): continue
+			line = line.rstrip()
+			if len(line) == 0: continue
+			fields = line.split()
 			if fields[0] in ('variableStep', 'fixedStep'): # beginning of a new chromosome
 				self._format = fields[0]
 				assert fields[1].startswith('chrom=')
@@ -211,12 +217,12 @@ class WiggleStream (FeatureStream):
 			
 			else: # parse a line of data
 				if self._format == 'variableStep':
-					assert len(fields) == 2
+					if len(fields) != 2: raise RuntimeError('bad format:\n%s' % line)
 					left_pos = int(fields[0])
 					right_pos = left_pos + self._span - 1
 					value = float(fields[1])
 				if self._format == 'fixedStep':
-					assert len(fields) == 1
+					if len(fields) != 1: raise RuntimeError('bad format:\n%s' % line)
 					left_pos = self._start + self._count_since_header * self._step
 					right_pos = left_pos + self._span - 1
 					value = float(fields[0])
@@ -235,27 +241,26 @@ class WiggleStream (FeatureStream):
 					for position_feature in full_feature: yield position_feature
 				else:
 					yield full_feature
-			
-	def _get_feature (self):
-		return next(self._feature_generator)
 
 
 class GffStream (FeatureStream):
 	'''
 	given an iterable of GFF-format lines (e.g. an opened GFF file), yield GenomeFeatures
 	'''
-	
-	def _get_feature (self):
-		fields = next(self.source).rstrip().split('\t')
-		while fields[0].startswith('#') or len(fields) < 8: # skip bad lines
-			fields = next(self.source).rstrip().split('\t')
-		return GenomeFeature(
-			reference_id =  self._get_reference_id(fields[0]),
-			left_pos =      int(fields[3]),
-			right_pos =     int(fields[4]),
-			is_reverse =    fields[6] == '-',
-			data =          fields
-		)
+	def _yield_features (self):
+		for line in self.source:
+			if line.startswith('#'): continue
+			line = line.rstrip()
+			if len(line) == 0: continue
+			fields = line.split('\t')
+			if len(fields) < 8: raise RuntimeError('bad format:\n%s' % line)
+			yield GenomeFeature(
+				reference_id =  self._get_reference_id(fields[0]),
+				left_pos =      int(fields[3]),
+				right_pos =     int(fields[4]),
+				is_reverse =    fields[6] == '-',
+				data =          fields
+			)
 
 
 class FastaStream (FeatureStream):
@@ -268,7 +273,7 @@ class FastaStream (FeatureStream):
 	handles buffering given the fact that FASTA is usually split across arbtirary line length
 	'''
 	
-	__slots__ = 'span', 'overlap', 'return_partial', '_sequence_buffer', '_left_pos', '_reference_id', '_feature_generator'
+	__slots__ = 'span', 'overlap', 'return_partial', '_sequence_buffer', '_left_pos', '_reference_id'
 	
 	def __init__ (self, *args, span = 1, overlap = False, return_partial = True, **kwargs):
 		super().__init__(*args, verify_order = False, **kwargs) # nonsensical to verify sorting because we're defining the coordinates
@@ -316,6 +321,4 @@ class FastaStream (FeatureStream):
 					yield self._create_feature(self.span)
 		
 		for feature in self._purge_buffer(): yield feature		
-	
-	def _get_feature (self):
-		return next(self._feature_generator)
+
