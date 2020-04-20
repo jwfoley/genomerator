6baf1bfb1aff2fcffc132a067deac651a59f1447
Joe Foley <jwfoley@stanford.edu>
2020-04-20 16:45:36 -0700

    add Buffer

--- genomerator/__init__.py
+++ genomerator/__init__.py
@@ -3,5 +3,6 @@ from .container import GenomeArray, GenomeBuffer
 from .input import read_references, read_reference_lengths, FeatureStream, SamStream, VariantStream, BedStream, BedgraphStream, WiggleStream, GffStream, GtfStream, FastaLineStream, SequenceStream, FastaStream
 from .output import Wiggler
 from .generator import FeatureOverlapper, RegionGenerator, OperationGenerator
+from .buffer import Buffer
 from .ProgressBar import ProgressBar
 
